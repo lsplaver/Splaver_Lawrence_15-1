120b105dc596f3daf3bd39c8599671ffa45720b6
Lawrence Splaver <lsplaver@yahoo.com>
2021-12-09 12:14:29 -0500

    Added and coded TotalWinnings component and TotalWinnings/TotalWinnings.  Updated Shared/_Layout to use the TotalWinnings view component.

---
+++ BlackJack/Components/TotalWinnings.cs
@@ -0,0 +1,20 @@
+﻿using BlackJack.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackJack.Components
+{
+    public class TotalWinnings : ViewComponent
+    {
+        private IGame game { get; set; }
+        public TotalWinnings(IGame g) => game = g;
+
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Views/Shared/Components/TotalWinnings/TotalWinnings.cshtml", game.Player.TotalWinnings);
+        }
+    }
+}
