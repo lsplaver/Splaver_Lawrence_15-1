8cfd9072e4292d0eeebc6d41840701a4712bcd56
Lawrence Splaver <lsplaver@yahoo.com>
2021-12-08 23:03:32 -0500

    Added and coded TagHelperExtensionMethods and TempMessageTagHelper.  Updated Views/_ViewImports and Shared/_Layout.  Updated newtonsoft package to 3.1.21.

---
+++ BlackJack/Models/ExtensionMethods/TagHelperExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackJack.Models.TagHelpers
+{
+    public static class TagHelperExtensionMethods
+    {
+        public static void AppendCssClass(this TagHelperAttributeList list, string newCssClasses)
+        {
+            string oldCssClasses = list["class"]?.Value?.ToString();
+            string cssClasses = string.IsNullOrEmpty(oldCssClasses) ? newCssClasses : $"{oldCssClasses} {newCssClasses}";
+            list.SetAttribute("class", cssClasses);
+        }
+
+        public static void BuildTag(this TagHelperOutput output, string tagName, string classNames)
+        {
+            output.TagName = tagName;
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.AppendCssClass(classNames);
+        }
+
+        public static void BuildLink(this TagHelperOutput output, string url, string className)
+        {
+            output.BuildTag("a", className);
+            output.Attributes.SetAttribute("href", url);
+        }
+    }
+}
---
+++ BlackJack/TagHelpers/TempMessageTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackJack.Models.TagHelpers
+{
+    [HtmlTargetElement("my-temp-message")]
+    public class TempMessageTagHelper : TagHelper
+    {
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext viewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var tempData = viewContext.TempData;
+            if (tempData.ContainsKey("message"))
+            {
+                output.BuildTag("h4", $"bg-{tempData["background"]} text-white p-2");
+                output.Content.SetContent(tempData["message"].ToString());
+            }
+            else
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
