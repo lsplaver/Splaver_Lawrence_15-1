da16fd61a48f6d5ba3a3ea4f22541088641f3482
Lawrence Splaver <lsplaver@yahoo.com>
2021-12-10 18:25:23 -0500

    Updated Home/Index and Hand to pass a hand model.

--- BlackJack/Components/Hand.cs
+++ BlackJack/Components/Hand.cs
@@ -12,12 +12,12 @@ namespace BlackJack.Components
         private IGame game { get; set; }
         public Hand(IGame g) => game = g;
 
-        public IViewComponentResult Invoke(string handType)
+        public IViewComponentResult Invoke(string handType, object hand)
         {
             HandViewModel vm = new HandViewModel();
             if (handType == "Player")
             {
-                Models.Hand playerHand = game.Player.Hand;
+                Models.Hand playerHand = (Models.Hand)hand; //game.Player.Hand;
                 vm.HandType = "Player";
                 vm.Hand = playerHand;
                 return View("~/Views/Shared/Components/Hand/Hand.cshtml", vm);
