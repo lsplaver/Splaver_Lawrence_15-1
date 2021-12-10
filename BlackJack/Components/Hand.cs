137855ad4e658da297bc676051c0dbc4fd27d676
Lawrence Splaver <lsplaver@yahoo.com>
2021-12-10 18:11:33 -0500

    Updated Index to use view component Hand and partial view Hand/Hand.  Renamed component PlayerHand to Hand.  Renamed Shared/Components/PlayerHand/PlayerHand to Shared/Components/Hand/Hand.  Added and coded HandViewModel.

---
+++ BlackJack/Components/Hand.cs
@@ -0,0 +1,34 @@
+﻿using BlackJack.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackJack.Components
+{
+    public class Hand : ViewComponent
+    {
+        private IGame game { get; set; }
+        public Hand(IGame g) => game = g;
+
+        public IViewComponentResult Invoke(string handType)
+        {
+            HandViewModel vm = new HandViewModel();
+            if (handType == "Player")
+            {
+                Models.Hand playerHand = game.Player.Hand;
+                vm.HandType = "Player";
+                vm.Hand = playerHand;
+                return View("~/Views/Shared/Components/Hand/Hand.cshtml", vm);
+            }
+            else
+            {
+                Models.Hand dealerHand = game.Dealer.Hand;
+                vm.HandType = "Dealer";
+                vm.Hand = dealerHand;
+                return View("~/Views/Shared/Components/Hand/Hand.cshtml", vm);
+            }
+        }
+    }
+}
---
+++ BlackJack/Models/ViewModels/HandViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackJack.Models
+{
+    public class HandViewModel
+    {
+        //public Dictionary<string, object> HandAndType { get; set; }
+        public string HandType { get; set; }
+        public Hand Hand { get; set; }
+    }
+}
